ccb45a53a1145b9b8e69c4a9f4cd019eed32648c
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-13 05:12:14 +0530

    Time: 25 ms (31.32%), Space: 54 MB (67.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,34 +2,51 @@ class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        int[][] dp=new int[n][m];
+        int[][] dp=new int[n+1][m+1];
 
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
         }
 
-        return sol(n-1, m-1, s1, s2, dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int take=0;
+                int left=0;
+                int right=0;
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    take=dp[i-1][j-1]+1;
+                }
+                left=dp[i-1][j];
+                right=dp[i][j-1];
+                dp[i][j]=Math.max(take, Math.max(left, right));
+            }
+        }
+
+        return dp[n][m];
     }
 
-    private int sol(int i, int j, String s1, String s2, int[][] dp){
-        if(i<0 || j<0){
+    private int sol(int i, int j, String s1, String s2, int[][] dp) {
+        if (i < 0 || j < 0) {
             return 0;
         }
 
-        if(dp[i][j]!=-1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        int take=0;
-        int leftMove=0;
-        int rightMove=0;
-        if(s1.charAt(i) == s2.charAt(j)){
-            take=sol(i-1, j-1, s1, s2, dp)+1;
-        }else{
-            leftMove=sol(i-1, j, s1, s2, dp);
-            rightMove=sol(i, j-1, s1, s2, dp);
+        int take = 0;
+        int leftMove = 0;
+        int rightMove = 0;
+        if (s1.charAt(i) == s2.charAt(j)) {
+            take = sol(i - 1, j - 1, s1, s2, dp) + 1;
+        } else {
+            leftMove = sol(i - 1, j, s1, s2, dp);
+            rightMove = sol(i, j - 1, s1, s2, dp);
         }
 
-        dp[i][j]=Math.max(take, Math.max(leftMove, rightMove));
+        dp[i][j] = Math.max(take, Math.max(leftMove, rightMove));
 
         return dp[i][j];
     }

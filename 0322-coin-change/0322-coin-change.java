5f5b804830518442bfa9355f016d8cbf2d018893
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-12 10:12:08 +0530

    Time: 18 ms (43.25%), Space: 46.3 MB (71.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        long[] dp= new long[amount+1];
+        int n = coins.length;
+        long[] dp = new long[amount + 1];
 
-        Arrays.fill(dp, -1);
+        dp[0] = 0;
 
-        long numCoins=sol(amount, n, coins, dp);
-
-        if(numCoins>=Integer.MAX_VALUE){
-            return -1;
+        for (int i = 1; i <= amount; i++) {
+            long val = (int)Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                if (coins[j] <= i) {
+                    val = Math.min(dp[i - coins[j]], val);
+                }
+            }
+            dp[i] = val+1;
         }
-        return (int)numCoins;
+
+        if (dp[amount] >= Integer.MAX_VALUE)
+            return -1;
+        return (int) dp[amount];
 
     }
 
-    private long sol(int amount, int n, int[] coins, long[] dp){
-        if(amount==0){
+    private long sol(int amount, int n, int[] coins, long[] dp) {
+        if (amount == 0) {
             return 0;
         }
 
-        if(dp[amount]!=-1){
+        if (dp[amount] != -1) {
             return dp[amount];
         }
 
-        long result=Integer.MAX_VALUE;
+        long result = Integer.MAX_VALUE;
 
-        for(int i=0;i<n ;i++){
-            if(coins[i]<=amount){
-                result= Math.min(sol(amount-coins[i], n, coins, dp), result);
+        for (int i = 0; i < n; i++) {
+            if (coins[i] <= amount) {
+                result = Math.min(sol(amount - coins[i], n, coins, dp), result);
             }
         }
 
-        dp[amount]=result+1L;
+        dp[amount] = result + 1L;
 
         return dp[amount];
 

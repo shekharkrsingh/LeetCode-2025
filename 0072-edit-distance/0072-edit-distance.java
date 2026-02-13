54ce1d9999cefff12e0e9907f94d96efa4686f9f
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-13 16:25:55 +0530

    Time: 5 ms (65.97%), Space: 47.5 MB (15.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,11 +5,30 @@ class Solution {
 
         int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
 
-        return sol(n - 1, m - 1, word1, word2, dp);
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int result=Integer.MAX_VALUE;
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    result=dp[i-1][j-1];
+                }else{
+                    result=Math.min(result, dp[i-1][j-1]+1);
+                    result=Math.min(result, dp[i][j-1]+1);
+                    result=Math.min(result, dp[i-1][j]+1);
+                }
+
+                dp[i][j]=result;
+            }
+        }
+
+        return dp[n][m];
     }
 
     private int sol(int i, int j, String word1, String word2, int[][] dp) {

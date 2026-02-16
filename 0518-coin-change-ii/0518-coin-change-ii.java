0b19a385a4206ecaabf50c9e23c3390597f8201b
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-16 05:49:28 +0530

    Time: 13 ms (44.95%), Space: 56.6 MB (50.9%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,13 +1,24 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
-        int[][] dp= new int[n][amount+1];
+        int[][] dp= new int[n+1][amount+1];
         
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
         }
 
-        return sol(n-1, amount, coins, n, dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                int count=0;
+                if(coins[i-1]<=j){
+                    count+=dp[i][j-coins[i-1]];
+                }
+                count+=dp[i-1][j];
+                dp[i][j]=count;
+            }
+        }
+
+        return dp[n][amount];
     }
 
     private int sol(int i, int amount, int[] coins, int n, int[][] dp){

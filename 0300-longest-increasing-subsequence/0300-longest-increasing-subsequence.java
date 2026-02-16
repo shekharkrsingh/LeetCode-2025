5aa752e2002c94e5217c41950ef7720ba391ce58
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-16 18:01:33 +0530

    Time: 37 ms (74.81%), Space: 46 MB (92.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,25 +2,21 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int[][] dp = new int[n + 1][n + 1];
+        int[] dp = new int[n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n-1; j >= -1; j--) {
-                int take = 0;
-                int notTake = dp[i + 1][j + 1];
-                if (j == -1 || nums[i] > nums[j]) {
-                    take = dp[i + 1][i + 1] + 1;
+        Arrays.fill(dp, 1);
+        int lng = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
                 }
-
-                dp[i][j + 1] = Math.max(take, notTake);
             }
+            lng = Math.max(lng, dp[i]);
         }
 
-        // for(int i=0;i<n+1; i++){
-        //     System.out.println(Arrays.toString(dp[i]));
-        // }
-        return dp[0][0];
-
+        return lng;
     }
 
     private int sol(int i, int j, int n, int[] nums, int[][] dp) {

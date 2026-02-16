3ac9b9e751af0ecc7f476eb8c62293c5d1fc47c1
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-16 16:05:32 +0530

    Time: 375 ms (12.81%), Space: 237.1 MB (19.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,45 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[][] dp=new int[n+1][n+1];
+        int n = nums.length;
 
-        
-        for (int i = 1; i <= n; i++) {
+        int[][] dp = new int[n + 1][n + 1];
 
-            for (int j = -1; j < n; j++) {
-
-                int notTake = dp[i - 1][j + 1];
-                int take = 0;
-
-                if (j == -1 || nums[i - 1] < nums[j]) {
-                    take = 1 + dp[i - 1][i];
-                }
-
-                dp[i][j + 1] = Math.max(take, notTake);
-            }
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        return dp[n][0];
+        return sol(0, -1, n, nums, dp);
     }
 
-    private int sol(int i, int j, int[] nums, int[][]dp){
-        if(i<0){
+    private int sol(int i, int j, int n, int[] nums, int[][] dp) {
+        if (i >= n) {
             return 0;
         }
 
-        if(dp[i][j+1]!=-1){
-            return dp[i][j+1];
+        if (dp[i][j + 1] != -1) {
+            return dp[i][j + 1];
         }
 
-        int count=0;
-
-        
-        if( j==-1 || nums[i]<nums[j]){
-            count=sol(i-1, i, nums, dp)+1;
+        int take = 0;
+        int notTake = sol(i + 1, j, n, nums, dp);
+        if (j == -1 || nums[i] > nums[j]) {
+            take = sol(i + 1, i, n, nums, dp) + 1;
         }
-        count=Math.max(count, sol(i-1, j, nums, dp));
 
-        dp[i][j+1]=count;
-        return count;
+        dp[i][j + 1] = Math.max(take, notTake);
+
+        return dp[i][j + 1];
     }
 }

570a3363ae314b5f1766a2ce5f23abb34fb14d61
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-19 08:26:41 +0530

    Time: 2 ms (13.52%), Space: 47.3 MB (10.71%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    int[] idx1 = { -1, 1, 0, 0 };
+    int[] idx2 = { 0, 0, -1, 1 };
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { sr, sc });
+        if (image[sr][sc] != color)
+            bfs(image, color, image[sr][sc], q, n, m);
+        return image;
+    }
+
+    private void bfs(int[][] image, int color, int start, Queue<int[]> q, int n, int m) {
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+
+            int i = node[0];
+            int j = node[1];
+
+
+            image[i][j] = color;
+
+            for (int k = 0; k < 4; k++) {
+                if (i + idx1[k] >= 0 && i + idx1[k] < n && j + idx2[k] >= 0 && j + idx2[k] < m
+                        && image[i + idx1[k]][j + idx2[k]] == start) {
+                    q.offer(new int[] { i + idx1[k], j + idx2[k] });
+                }
+            }
+        }
+    }
+}

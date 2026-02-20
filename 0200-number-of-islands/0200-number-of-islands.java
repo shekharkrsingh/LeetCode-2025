a997a45f2e0233bf911a62ca8acc0ad8d48069a8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-20 10:26:02 +0530

    Time: 7 ms (15.18%), Space: 51.7 MB (81.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,44 @@
 class Solution {
+    private static int[] dx = { -1, 1, 0, 0 };
+    private static int[] dy = { 0, 0, 1, -1 };
+
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int count = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        boolean[][] visited = new boolean[n][m];
+        int cnt = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    grid[i][j]='2';
-                    count++;
-                    dfs(i, j, n, m, grid);
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    cnt++;
+                    visited[i][j] = true;
+                    q.offer(new int[] { i, j });
+                    bfs(n, m, grid, visited, q);
                 }
             }
         }
-        return count;
+
+        return cnt;
     }
 
-    private void dfs(int i, int j, int n, int m,  char[][] grid) {
-        if ((i - 1) >= 0 && grid[i - 1][j] == '1') {
-            grid[i-1][j]='2';
-            dfs(i - 1, j, n, m, grid);
-        }
-        if ((j - 1) >= 0 && grid[i][j - 1] == '1') {
-            grid[i][j-1]='2';
-            dfs(i, j - 1, n, m, grid);
-        }
-        if ((i + 1) < n && grid[i + 1][j] == '1') {
-            grid[i+1][j]='2';
-            dfs(i + 1, j, n, m, grid);
-        }
-        if ((j + 1) < m && grid[i][j + 1] == '1') {
-            grid[i][j+1]='2';
-            dfs(i, j + 1, n, m, grid);
+    private void bfs(int n, int m, char[][] grid, boolean[][] visited, Queue<int[]> q) {
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int x = node[0];
+            int y = node[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] == '1' && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.offer(new int[] { nx, ny });
+                }
+            }
         }
     }
 }

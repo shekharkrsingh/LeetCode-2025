de0d78786a0302dac5dc228530cd0dae32b7696a
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-21 12:48:37 +0530

    Time: 8 ms (30.93%), Space: 47.3 MB (49.65%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n=prerequisites.length; 
+        
+        boolean[] visited = new boolean[numCourses];
+        List<List<Integer>> adj = adjecencyList(numCourses, prerequisites);
+        List<Integer> topo= new ArrayList<>();
+        Queue<Integer> q= new LinkedList<>();
+        int[] inDegree= new int[numCourses];
+
+        for(int i=0;i<n;i++){
+            inDegree[prerequisites[i][1]]++;
+        }
+
+        for(int i=0;i<numCourses; i++){
+            if(inDegree[i]==0){
+                q.offer(i);
+            }
+        }
+
+        bfs(q, adj, topo, inDegree);
+
+        if(topo.size()!=numCourses){
+            return false;
+        }
+
+        return true;
+    }
+
+    private void bfs(Queue<Integer> q, List<List<Integer>> adj, List<Integer> topo, int[] inDegree){
+        while(!q.isEmpty()){
+
+            int node=q.poll();
+            topo.add(node);
+            List<Integer> list= adj.get(node);
+
+            for(int i: list){
+                inDegree[i]--;
+                if(inDegree[i]==0){
+                    q.offer(i);
+                }
+            }
+
+        }
+    }
+
+
+    private List<List<Integer>> adjecencyList(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] arr : edges) {
+            adj.get(arr[0]).add(arr[1]);
+        }
+
+        return adj;
+    }
+}

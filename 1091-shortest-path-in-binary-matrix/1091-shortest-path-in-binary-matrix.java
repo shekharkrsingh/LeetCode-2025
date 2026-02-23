8fb895ac6bc44238d7e4b4b9b73918beb7deda60
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-02-23 15:41:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private static int[] dx = { -1, -1, -1, 1, 1, 1, 0, 0, 0 };
+    private static int[] dy = { 0, 1, -1, 0, 1, -1, 0, 1, -1 };
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+
+        boolean[][] visited = new boolean[n][n];
+        int[][] dis = new int[n][n];
+        Queue<int[]> q = new LinkedList<>();
+
+        dis[0][0] = 1;
+        visited[0][0]=true;
+
+        q.offer(new int[] { 0, 0 });
+
+        bfs(q, grid, n, visited, dis);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(grid[i]));
+        }
+
+        return dis[n - 1][n - 1];
+    }
+
+    private void bfs(Queue<int[]> q, int[][] grid, int n, boolean[][] visited, int[][] dis) {
+        while (!q.isEmpty()) {
+
+            int[] node = q.poll();
+
+            int x = node[0];
+            int y = node[1];
+
+            for (int i = 0; i < 9; i++) {
+
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && grid[nx][ny] == 0 && !visited[nx][ny]) {
+
+                    visited[nx][ny] = true;
+                    dis[nx][ny] = dis[x][y] + 1;
+
+                    q.offer(new int[] { nx, ny });
+                }
+            }
+        }
+    }
+}

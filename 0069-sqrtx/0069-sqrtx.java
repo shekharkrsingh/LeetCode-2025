bedb886880f776167e210198a68a65bae902502f
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-11 15:52:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int lb = 0;
+        int start = 0;
+        int end = x / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int product = mid * mid;
+            if (product == x) {
+                return mid;
+            }
+            if (product > x) {
+                end = mid - 1;
+            } else {
+                lb = mid;
+                start = mid + 1;
+            }
+        }
+        return lb;
     }
 }

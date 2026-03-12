22dd21f9e6046a1f933b347bc35167b6ef7d0298
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-12 15:25:56 +0530

    Time: 7 ms (81.22%), Space: 47.8 MB (85.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
         int max = Integer.MIN_VALUE;
+
         for (int banana : piles) {
             max = Math.max(banana, max);
         }
+
         int start = 1;
         int end = max;
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            int cnt = 0;
-            for (int i = 0; i < n; i++) {
-                cnt += piles[i] / mid;
-                if (piles[i] % mid != 0) {
-                    cnt++;
-                }
+
+            long cnt = 0;
+            for (int pile : piles) {
+                cnt += (pile + mid - 1) / mid; 
             }
+
             if (cnt <= h) {
                 max = mid;
                 end = mid - 1;
@@ -23,18 +24,7 @@ class Solution {
                 start = mid + 1;
             }
         }
+
         return max;
     }
 }
-// Current:
-// Binary Search
-// /
-// Greedy
-// Suggested:
-// Binary Search
-// /
-// Greedy
-// Key Idea:
-// Using binary search on the answer range to find the minimum eating speed satisfying the time constraint.
-// Consider:
-// Since you nailed the logic, could you optimize the ceiling division calculation to avoid the explicit if-check inside the loop?

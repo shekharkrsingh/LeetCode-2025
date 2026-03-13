c699b98998956a34056cb05ea701490b6d401807
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-13 10:05:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,39 +1,51 @@
 class Solution {
+
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
-        if (n < (long) m * k) {
+
+        if (n < (long) m * k)
             return -1;
-        }
-        int maxDay = 0;
-        int minDay = bloomDay[0];
+
+        int minDay = bloomDay[0], maxDay = bloomDay[0];
         for (int day : bloomDay) {
-            maxDay = Math.max(maxDay, day);
             minDay = Math.min(minDay, day);
+            maxDay = Math.max(maxDay, day);
         }
-        int start = minDay;
-        int end = maxDay;
+
+        int start = minDay, end = maxDay;
+        int ans = -1;
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            int cnt = 0;
-            int total = 0;
-            for (int i = 0; i < n; i++) {
-                if (bloomDay[i] <= mid) {
-                    total++;
-                } else {
-                    total = 0;
-                }
-                if (total == k) {
-                    cnt++;
-                    total = 0;
-                }
-            }
-            if (cnt >= m) {
-                maxDay = mid;
+
+            if (canMake(bloomDay, mid, m, k)) {
+                ans = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-        return maxDay;
+
+        return ans;
+    }
+
+    private boolean canMake(int[] bloomDay, int day, int m, int k) {
+        int flowers = 0, bouquets = 0;
+
+        for (int bloom : bloomDay) {
+            if (bloom <= day) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                    if (bouquets >= m)
+                        return true; // early stop
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        return false;
     }
 }

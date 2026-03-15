618442c5f85c73d8a47209f0842738422d14e018
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-15 20:54:22 +0530

    Time: 15 ms (82.16%), Space: 46.3 MB (81.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,46 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        long[] dp = new long[amount + 1];
-
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 100_000);
         dp[0] = 0;
-
         for (int i = 1; i <= amount; i++) {
-            long val = (int)Integer.MAX_VALUE;
             for (int j = 0; j < n; j++) {
-                if (coins[j] <= i) {
-                    val = Math.min(dp[i - coins[j]], val);
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
-            dp[i] = val+1;
         }
-
-        if (dp[amount] >= Integer.MAX_VALUE)
-            return -1;
-        return (int) dp[amount];
-
-    }
-
-    private long sol(int amount, int n, int[] coins, long[] dp) {
-        if (amount == 0) {
-            return 0;
-        }
-
-        if (dp[amount] != -1) {
-            return dp[amount];
-        }
-
-        long result = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            if (coins[i] <= amount) {
-                result = Math.min(sol(amount - coins[i], n, coins, dp), result);
-            }
-        }
-
-        dp[amount] = result + 1L;
-
-        return dp[amount];
-
+        return dp[amount] >= 100_000 ? -1 : dp[amount];
     }
 }

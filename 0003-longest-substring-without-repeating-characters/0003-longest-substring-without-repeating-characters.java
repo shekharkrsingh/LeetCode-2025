182e8016f8360c735e88b894a64077cb25df8e93
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-23 15:20:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -8,16 +8,8 @@ class Solution {
          for(int i=0;i<s.length();i++){
                char ch=s.charAt(i);
                if(st.contains(ch)){
-                  while(true){
-                      char ch1=s.charAt(j);
-                      if(ch1==ch){
-                          st.remove(ch1);
-                          j++;
-                          break;
-                      }
-                      st.remove(ch1);
-                      j++;
-                  }
+                  st.remove(s.charAt(j));
+                  j++;
                }
                st.add(ch);
                ans=Math.max(ans,st.size());

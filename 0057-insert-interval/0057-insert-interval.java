5cb27cf1111d6c6f5dd6649b2e45eb1601bbe2da
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-24 05:45:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,39 +1,34 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n=intervals.length;
-        boolean flag=true;
-        List<int[]> list=new ArrayList<>();
-        List<int[]> merge=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(flag && intervals[i][0]>newInterval[0]){
-                list.add(new int[]{newInterval[0], newInterval[1]});
-                flag=false;
+        List<int[]> list = new ArrayList<>();
+        boolean inserted = false;
+
+        for (int[] interval : intervals) {
+            if (!inserted && interval[0] > newInterval[0]) {
+                list.add(newInterval);
+                inserted = true;
             }
-            list.add(new int[]{intervals[i][0], intervals[i][1]});
-        }
-        if(flag){
-            list.add(new int[]{newInterval[0], newInterval[1]});
+            list.add(interval);
         }
 
-        int s=list.size();
-        merge.add(list.get(0));
+        if (!inserted) {
+            list.add(newInterval);
+        }
 
-        for(int i=1;i<s;i++){
-            int len=merge.size();
-            if(merge.get(len-1)[1]>=list.get(i)[0]){
-                merge.set(len-1, new int[]{merge.get(len-1)[0], (int)Math.max(list.get(i)[1], merge.get(len-1)[1])});
-            }else{
-                merge.add(list.get(i));
+        List<int[]> merged = new ArrayList<>();
+        merged.add(list.get(0));
+
+        for (int i = 1; i < list.size(); i++) {
+            int[] last = merged.get(merged.size() - 1);
+            int[] curr = list.get(i);
+
+            if (last[1] >= curr[0]) {
+                last[1] = Math.max(last[1], curr[1]); // merge
+            } else {
+                merged.add(curr);
             }
         }
 
-         n=merge.size();
-        int[][] res=new int[n][2];
-
-        for(int i=0;i<merge.size();i++){
-            res[i]=merge.get(i);
-        }
-
-        return res;
+        return merged.toArray(new int[merged.size()][]);
     }
 }

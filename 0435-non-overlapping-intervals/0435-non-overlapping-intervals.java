4f93f8786ba350c57f35e7059a86d9a8900512e8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-24 15:31:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -2,27 +2,30 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         int n = intervals.length;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        int prev = Integer.MIN_VALUE;
+        int prev= Integer.MIN_VALUE;
+        // int prev2=Integer.MIN_VALUE;
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             if(prev>intervals[i][0]){
+                prev=Math.min(prev, intervals[i][1]);
                 cnt++;
             }else{
                 prev=intervals[i][1];
             }
         }
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        prev = Integer.MIN_VALUE;
-        int cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (intervals[i][0] < prev) {
-                cnt2++;
-            } else {
-                prev = intervals[i][1];
-            }
-        }
+        // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        // prev = Integer.MIN_VALUE;
+        // int cnt2 = 0;
+        // for (int i = 0; i < n; i++) {
+        //     if (intervals[i][0] < prev) {
+        //         cnt2++;
+        //     } else {
+        //         prev = intervals[i][1];
+        //     }
+        // }
 
-        return Math.min(cnt, cnt2);
+        // return Math.min(cnt, cnt2);
+        return cnt;
     }
 }

6d935e4443be97f8615aebde26d87858f212cdf8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-25 11:18:04 +0530

    Time: 262 ms (5.59%), Space: 172.7 MB (5.59%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        long[] h = new long[n];
+        long[] v = new long[m];
+
+        long totalSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += grid[i][j];
+                v[j] += grid[i][j];
+                totalSum += grid[i][j];
+            }
+            h[i] = sum;
+        }
+
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        long check = totalSum / 2;
+
+        System.out.println(check);
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0)
+                h[i] += h[i - 1];
+            if (h[i] == check)
+                return true;
+            System.out.println(h[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (i != 0)
+                v[i] += v[i - 1];
+            if (v[i] == check)
+                return true;
+        }
+
+        return false;
+    }
+}

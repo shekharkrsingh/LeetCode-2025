546252118814106974ef2ca9ea811505b92d3eb2
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-30 01:37:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -10,10 +10,7 @@ class Solution {
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    break;
-                }
+            for (int j = 0; j < i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;

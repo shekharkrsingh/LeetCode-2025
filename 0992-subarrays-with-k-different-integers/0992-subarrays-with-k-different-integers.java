736ecd798fc274bd1a7b99668862ea17653708ce
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-29 12:14:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -10,21 +10,16 @@ class Solution {
 
         Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        int cnt = 0;
         int j = 0;
         int res = 0;
 
         for (int i = 0; i < n; i++) {
-            if (!map.containsKey(nums[i])) {
-                cnt++;
-            }
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
-            while (cnt > k) {
+            while (map.size() > k) {
                 int val = map.get(nums[j]);
                 if (val == 1) {
                     map.remove(nums[j]);
-                    cnt--;
                 } else {
                     map.put(nums[j], val - 1);
                 }

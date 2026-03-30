b167bb3841b9bb3d677d242389c1b51085651857
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-03-30 15:02:12 +0530

    Time: 2 ms (99.87%), Space: 52 MB (84.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,61 +1,42 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int a = Integer.MIN_VALUE;
-        int b = Integer.MIN_VALUE;
 
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int a = 0, b = 0;
+        int cnt1 = 0, cnt2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && nums[i] != b) {
-                a = nums[i];
+        for (int num : nums) {
+            if (num == a) {
                 cnt1++;
-                continue;
-            }
-            if (cnt2 == 0 && nums[i] != a) {
-                b = nums[i];
+            } else if (num == b) {
                 cnt2++;
-                continue;
-            }
-
-            if (nums[i] != a) {
-                if (nums[i] == b) {
-                    cnt2++;
-                } else {
-                    cnt2--;
-                }
-            }
-
-            if (nums[i] != b) {
-                if (nums[i] == a) {
-                    cnt1++;
-                } else {
-                    cnt1--;
-                }
+            } else if (cnt1 == 0) {
+                a = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                b = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
 
         cnt1 = 0;
         cnt2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == a) {
-                cnt1++;
-            }
-            if (nums[i] == b) {
-                cnt2++;
-            }
+        for (int num : nums) {
+            if (num == a) cnt1++;
+            else if (num == b) cnt2++;
         }
 
-        List<Integer> list = new ArrayList<>();
-        if (cnt1 > n / 3) {
-            list.add(a);
-        }
-        if (cnt2 > n / 3) {
-            list.add(b);
-        }
+        List<Integer> result = new ArrayList<>();
 
-        return list;
+        if (cnt1 > n / 3) result.add(a);
+        if (cnt2 > n / 3) result.add(b);
+
+        return result;
     }
 }

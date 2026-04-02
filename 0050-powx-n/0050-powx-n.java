4bfa0ae0f112c34daa0b7b9043f2d4895f03dc41
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-02 16:43:13 +0530

    Time: 0 ms (100%), Space: 47.7 MB (75.69%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n > 0) {
-            return gPow(x, n);
+        long N = n; 
+        if (N >= 0) {
+            return gPow(x, N);
         }
-        return 1 / gPow(x, (-1) * n);
+        return 1 / gPow(x, -N);
     }
 
-    private double gPow(double x, int n) {
-        if (n == 1) {
-            return x;
-        }
+    private double gPow(double x, long n) {
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+
         if (n % 2 == 0) {
             return gPow(x * x, n / 2);
         } else {
-            return x * gPow(x, n - 1);
+            return x * gPow(x * x, n / 2);
         }
     }
 }

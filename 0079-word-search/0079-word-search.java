0fa68eb9df1e80bac157d6de0283f36a5e09eaf2
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-05 15:07:13 +0530

    Time: 184 ms (22.01%), Space: 43 MB (51.48%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,50 @@
+class Solution {
+    private static final int[] dx = { 1, 0, -1, 0 };
+    private static final int[] dy = { 0, 1, 0, -1 };
+
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+
+        boolean[][] visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, 0, i, j, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int idx, int i, int j, boolean[][] visited) {
+        if (idx == word.length()) {
+            return true; // All characters matched
+        }
+
+        int n = board.length;
+        int m = board[0].length;
+
+        if (i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || board[i][j] != word.charAt(idx)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        for (int dir = 0; dir < 4; dir++) {
+            int ni = i + dx[dir];
+            int nj = j + dy[dir];
+
+            if (dfs(board, word, idx + 1, ni, nj, visited)) {
+                return true;
+            }
+        }
+
+        visited[i][j] = false; // backtrack
+        return false;
+    }
+}

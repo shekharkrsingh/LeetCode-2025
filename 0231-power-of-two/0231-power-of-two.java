02ce1c43f68ad684b2b8345ffbbdb6b46b334b89
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-06 12:50:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0)
-        return false;
-        return (n&n-1)==0;
+        if (n <= 0)
+            return false;
+        return (n & n - 1) == 0;
     }
 }

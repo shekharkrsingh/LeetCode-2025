5d1bb22fd2e83ca3d3b4a978adda8117b483b0af
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-07 12:52:27 +0530

    Time: 63 ms (72.03%), Space: 56.4 MB (90.21%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,105 @@
+class Robot {
+
+    private Integer width;
+    private Integer height;
+
+    private Integer x=0;
+    private Integer y=0;
+
+    private String dir;
+
+    public Robot(int width, int height) {
+        this.width=width-1;
+        this.height=height-1;
+        this.dir="East";
+    }
+    
+    public void step(int num) {
+        int perimeter = 2 * (height + width);
+        if (num >= perimeter) {
+            num = num % perimeter;
+            if (num == 0) {
+                num = perimeter; 
+            }
+        }
+        if(x==width && dir=="East"){
+            dir="North";
+            step(num);
+            return;
+        }
+        if(x==0 && dir=="West"){
+            dir="South";
+            step(num);
+            return;
+        }
+        if(y==height && dir=="North"){
+            dir="West";
+            step(num);
+            return;
+        }
+        if(y==0 && dir=="South"){
+            dir="East";
+            step(num);
+            return;
+        }
+
+        if(dir=="East" && x+num>width){
+            int val=num-width+x;
+            x=width;
+            step(val);
+            return;
+        }
+        if(dir=="West" && x<num){
+            int val=num-x;
+            x=0;
+            step(val);
+            return;
+        }
+        if(dir=="North" && y+num>height){
+            int val=num-height+y;
+            y=height;
+            step(val);
+            return;
+        }
+        if(dir=="South" && y<num){
+            int val=num-y;
+            y=0;
+            step(val);
+            return;
+        }
+
+        if(dir=="East" && x+num<=width){
+            x+=num;
+            return;
+        }
+        if(dir=="West" && x>=num){
+            x=x-num;
+            return;
+        }
+        if(dir=="North" && y+num<=height){
+            y+=num;
+            return;
+        }
+        if(dir=="South"&& y>=num){
+            y-=num;
+            return;
+        }
+
+    }
+    
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+    
+    public String getDir() {
+        return this.dir;
+    }
+}
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */

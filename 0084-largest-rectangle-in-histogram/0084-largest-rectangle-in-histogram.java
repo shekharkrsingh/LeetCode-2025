c95e4d5b2085d357c796c94ea51994eb05234aa0
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-15 12:13:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -12,34 +12,42 @@ class Solution {
                 st.poll();
             }
 
-            if (st.isEmpty()) {
-                pre[i] = -1;
-            } else {
-                pre[i] = st.peek();
+            // if (st.isEmpty()) {
+            //     pre[i] = -1;
+            // } else {
+            //     pre[i] = st.peek();
+            // }
+
+            if(!st.isEmpty()){
+                res=Math.max(res, (i-st.peek())*heights[i]);
+            }else{
+                System.out.println("ddfdfdfd");
+                res=Math.max(res, i*heights[i]);
             }
+
 
             st.push(i);
         }
 
-        st.clear();
+        // st.clear();
 
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.poll();
-            }
+        // for (int i = n - 1; i >= 0; i--) {
+        //     while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        //         st.poll();
+        //     }
 
-            if (st.isEmpty()) {
-                post[i] = n;
-            } else {
-                post[i] = st.peek();
-            }
+        //     if (st.isEmpty()) {
+        //         post[i] = n;
+        //     } else {
+        //         post[i] = st.peek();
+        //     }
 
-            st.push(i);
-        }
+        //     st.push(i);
+        // }
 
-        for (int i = 0; i < n; i++) {
-            res = Math.max(res, (post[i] - pre[i] - 1) * heights[i]);
-        }
+        // for (int i = 0; i < n; i++) {
+        //     res = Math.max(res, (post[i] - pre[i] - 1) * heights[i]);
+        // }
 
         return res;
 

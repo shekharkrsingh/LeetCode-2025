e510318f19830926ff08f653cbb6168b58bb41f2
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-15 11:21:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,22 +1,18 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        int res = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
+
             if (words[(startIndex + i) % n].equals(target)) {
-                res = i;
-                break;
+                return i;
             }
-        }
 
-        for (int i = 0; i < n; i++) {
             if (words[(startIndex - i + n) % n].equals(target)) {
-                res = Math.min(res, i);
-                break;
+                return i;
             }
         }
 
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return -1;
     }
 }

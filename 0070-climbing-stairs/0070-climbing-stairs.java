768df59379498f3856e10458ce4e030ac0571c00
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-29 12:14:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-
-        dp[0]=1;
-        dp[1]=1;
+        int first=1;
+        int second=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=first+second;
+            first=second;
+            second=temp;
         }
-
-        return dp[n];
+        return second;
     }
 
     private int sol(int n, int[] dp) {

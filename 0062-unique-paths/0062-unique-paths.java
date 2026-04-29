3f88f887aacbbc45e6f8892ddf1921502c824b70
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-04-29 15:00:02 +0530

    Time: 0 ms (100%), Space: 41.9 MB (90.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,35 +1,30 @@
 class Solution {
     public int uniquePaths(int n, int m) {
-        int[] prev= new int[m];
-
-        Arrays.fill(prev, 1);
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                prev[j]=prev[j-1]+ prev[j];
-            }
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        return prev[m-1];
+        return sol(n - 1, m - 1, n - 1, m - 1, dp);
     }
 
-    private int sol(int i, int j, int n, int m, int[][] dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
+    private int sol(int i, int j, int n, int m, int[][] dp) {
+        if (i < 0 || j < 0) {
             return 0;
         }
+        if (i == 0 && j == 0) {
+            return 1;
+        }
 
-        if(dp[i][j]!=-1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int up=sol(i-1, j, n, m, dp);
-        int down=sol(i, j-1, n, m, dp);
+        int total = 0;
+        total += sol(i - 1, j, n, m, dp);
+        total += sol(i, j - 1, n, m, dp);
 
-        dp[i][j]=up+down;
-
-        return dp[i][j];
+        dp[i][j] = total;
+        return total;
     }
 }

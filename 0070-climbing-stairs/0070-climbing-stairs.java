ab5f3b3535b7bfb50819eb717818684502433656
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-13 15:49:59 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,28 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int first=1;
-        int second=1;
-        for(int i=2;i<=n;i++){
-            int temp=first+second;
-            first=second;
-            second=temp;
+        int first = 1, second = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = first + second;
+            second = first;
+            first = temp;
         }
-        return second;
-    }
-
-    private int sol(int n, int[] dp) {
-        if (n == 0) {
-            return 1;
-        }
-        if (n < 0) {
-            return 0;
-        }
-
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-
-        dp[n] = sol(n - 1, dp) + sol(n - 2, dp);
-        return dp[n];
+        return first;
     }
 }

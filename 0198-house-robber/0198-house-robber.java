880796fcdd81206a34a2ad94a54e8bc866d0caa8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-16 10:43:21 +0530

    Time: 0 ms (100%), Space: 42.4 MB (96.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,8 +2,20 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return sol(n - 1, nums, dp);
+
+        if(n==1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int take = dp[i - 2] + nums[i];
+            int notTake = dp[i - 1];
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[n-1];
     }
 
     private int sol(int n, int[] nums, int[] dp) {
@@ -14,7 +26,7 @@ class Solution {
             return nums[n];
         }
 
-        if(dp[n]!=-1){
+        if (dp[n] != -1) {
             return dp[n];
         }
 

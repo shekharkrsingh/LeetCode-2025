db1e9c9c98f698b2573b5caca3748a32472ce085
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-16 10:57:14 +0530

    Time: 0 ms (100%), Space: 42 MB (80.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,42 +1,31 @@
 class Solution {
-    public int uniquePaths(int n, int m) {
-        int[] curr = new int[m];
-        int[] prev = new int[m];
-        Arrays.fill(prev, 1);
-        curr[0] = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                int total = 0;
-                total += prev[j];
-                total += curr[j - 1];
-                curr[j] = total;
-            }
-
-            for (int j = 1; j < m; j++) {
-                prev[j] = curr[j];
-            }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
         }
+        return sol(m - 1, n - 1, dp);
 
-        return prev[m - 1];
     }
 
-    private int sol(int i, int j, int n, int m, int[][] dp) {
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-        if (i == 0 && j == 0) {
+    private int sol(int m, int n, int[][] dp) {
+        if (m == 0 && n == 0) {
             return 1;
         }
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+        int up = 0;
+        int left = 0;
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (n - 1 >= 0) {
+            up = sol(m, n - 1, dp);
+        }
+        if (m - 1 >= 0) {
+            left = sol(m - 1, n, dp);
         }
 
-        int total = 0;
-        total += sol(i - 1, j, n, m, dp);
-        total += sol(i, j - 1, n, m, dp);
-
-        dp[i][j] = total;
-        return total;
+        dp[m][n] = left + up;
+        return dp[m][n];
     }
 }

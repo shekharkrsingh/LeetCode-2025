9f6b854aaebdc78a4a9df019871fccf63dd264fc
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-16 14:49:22 +0530

    Time: 46 ms (58.02%), Space: 71.7 MB (36.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,13 +5,30 @@ class Solution {
         for( int val: nums){
             sum+=val;
         }
-
         if(sum%2!=0){
             return false;
         }
-        int[][] dp =new int[n][(sum/2)+1];
+        sum=sum/2;
+        int[][] dp =new int[n+1][sum+1];
+        Arrays.fill(dp[0], -1);
+        for(int i=1;i<n;i++){
+            dp[i][0]=1;
+        }
 
-        return sol(n-1, sum/2, nums, dp)==1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                int take=0;
+                if(j-nums[i-1]>=0){
+                    take=dp[i][j-nums[i-1]];
+                }
+                int notTake=dp[i][j];
+
+                dp[i+1][j]=take==1 || notTake==1?1:-1;
+            }
+        }
+
+        return dp[n][sum]==1;
+
     }
 
     private int sol(int i, int sum, int[] nums, int[][] dp){

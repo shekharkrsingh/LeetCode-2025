74d5bfa7c0e814c7cb0e8c038ec9f91dc44922f2
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-16 10:35:10 +0530

    Time: 0 ms (100%), Space: 42.7 MB (55.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,28 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int[] dp= new int[n+1];
+        int n = nums.length;
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int take= nums[i];
-            if(i>1){
-                take=dp[i-2]+nums[i];
-            }
-            int notTake=dp[i-1];
-
-            dp[i]=Math.max(take, notTake);
-        }
-
-        return dp[n-1];
-
+        return sol(n - 1, nums, dp);
     }
 
-    private int sol(int n, int[] nums, int[]dp){
-        if(n<0) return 0;
-        if(dp[n]!=-1)return dp[n];
-        int take=sol(n-2, nums, dp)+nums[n];
-        int notTake=sol(n-1, nums, dp);
-        return dp[n]=Math.max(take, notTake);
+    private int sol(int n, int[] nums, int[] dp) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return nums[n];
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        int take = sol(n - 2, nums, dp) + nums[n];
+        int notTake = sol(n - 1, nums, dp);
+
+        int max = Math.max(take, notTake);
+
+        dp[n] = max;
+        return max;
+
     }
 }

0b773c01c15417262d3c79a6e449417357acea5a
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-23 12:23:43 +0530

    Time: 24 ms (30.93%), Space: 46.6 MB (37.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, 100_000);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i - coins[j] >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
+        int n=coins.length;
+        int[]dp = new int[amount+1];
+
+        Arrays.fill(dp, -1);
+
+        int val= sol(amount, coins, dp);
+        return val>=100000?-1:val;
+
+    }
+
+    private int sol(int i, int[] coins, int[]dp){
+        if(i==0){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int min=100000;
+
+        for(int j=0;j<coins.length;j++){
+            if(i-coins[j]>=0){
+                min=Math.min(min, sol(i-coins[j], coins, dp)+1);
             }
         }
-        return dp[amount] >= 100_000 ? -1 : dp[amount];
+
+        dp[i]=min;
+        return min;
     }
 }

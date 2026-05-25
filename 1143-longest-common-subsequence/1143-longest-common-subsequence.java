d2a84714d09e874b490ca2ca192605cddb38cca8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-25 14:50:06 +0530

    Time: 26 ms (23.54%), Space: 53.6 MB (87.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,52 +1,36 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
 
-        int[] curr= new int[m+1];
-        int[] prev= new int[m+1];
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                int take=0;
-                int left=0;
-                int right=0;
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    take=prev[j-1]+1;
-                }
-                left=prev[j];
-                right=curr[j-1];
-                curr[j]=Math.max(take, Math.max(left, right));
-            }
-
-            for(int j=1;j<=m;j++){
-                prev[j]=curr[j];
-            }
+        int[][] dp = new int[n1][n2];
+        for(int i=0;i<n1;i++){
+            Arrays.fill(dp[i], -1);
         }
 
-        return curr[m];
+        return sol(n1-1, n2-1, text1, text2, dp);
+        
     }
 
     private int sol(int i, int j, String s1, String s2, int[][] dp) {
-        if (i < 0 || j < 0) {
+        if (i < 0 || j < 0)
             return 0;
-        }
 
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        int take = 0;
-        int leftMove = 0;
-        int rightMove = 0;
+
+        int cnt = 0;
+
         if (s1.charAt(i) == s2.charAt(j)) {
-            take = sol(i - 1, j - 1, s1, s2, dp) + 1;
+            cnt = sol(i - 1, j - 1, s1, s2, dp) + 1;
         } else {
-            leftMove = sol(i - 1, j, s1, s2, dp);
-            rightMove = sol(i, j - 1, s1, s2, dp);
+            cnt = Math.max(sol(i - 1, j, s1, s2, dp), sol(i, j - 1, s1, s2, dp));
         }
 
-        dp[i][j] = Math.max(take, Math.max(leftMove, rightMove));
+        dp[i][j] = cnt;
 
-        return dp[i][j];
+        return cnt;
+
     }
 }

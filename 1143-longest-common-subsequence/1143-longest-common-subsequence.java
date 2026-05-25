1d2fdd423af07b45fec4472adc4d5d62f9121863
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-05-25 15:05:56 +0530

    Time: 18 ms (93.73%), Space: 42.9 MB (95.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,30 +3,49 @@ class Solution {
         int n1 = text1.length();
         int n2 = text2.length();
 
-        int[][] dp = new int[n1+1][n2+1];
+        // int[][] dp = new int[n1+1][n2+1];
 
-        for(int i=0;i<=n1;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=n2;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                int cnt=0;
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    cnt=dp[i-1][j-1]+1;
-                }else{
-                    cnt=Math.max(dp[i-1][j], dp[i][j-1]);
+        // for(int i=0;i<=n1;i++){
+        //     dp[i][0]=0;
+        // }
+        // for(int i=0;i<=n2;i++){
+        //     dp[0][i]=0;
+        // }
+        // for(int i=1;i<=n1;i++){
+        //     for(int j=1;j<=n2;j++){
+        //         int cnt=0;
+        //         if(text1.charAt(i-1)==text2.charAt(j-1)){
+        //             cnt=dp[i-1][j-1]+1;
+        //         }else{
+        //             cnt=Math.max(dp[i-1][j], dp[i][j-1]);
+        //         }
+
+        //         dp[i][j]=cnt;
+        //     }
+        // }
+
+        // return dp[n1][n2];
+
+        int[] curr = new int[n2 + 1];
+        int[] prev = new int[n2 + 1];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                int cnt = 0;
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    cnt = prev[j - 1] + 1;
+                } else {
+                    cnt = Math.max(prev[j], curr[j - 1]);
                 }
 
-                dp[i][j]=cnt;
+                curr[j] = cnt;
+            }
+            for (int j = 1; j <= n2; j++) {
+                prev[j] = curr[j];
             }
         }
 
-        return dp[n1][n2];
-        
-        
+        return prev[n2];
+
     }
 
     private int sol(int i, int j, String s1, String s2, int[][] dp) {

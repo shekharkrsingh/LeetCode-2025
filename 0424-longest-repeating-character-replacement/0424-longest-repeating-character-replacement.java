c1e43a592077f847ddfb37c374ce3db432352411
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-10 18:03:00 +0530

    Time: 102 ms (5.02%), Space: 47.2 MB (16.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,24 +1,42 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n=s.length();
-        int[] freq = new int[26];
-        int j = 0;
-        int maxFreq = 0;
-        int res = 0;
-
-        for (int i = 0; i < n; i++) {
-            int idx = s.charAt(i) - 'A';
-            freq[idx]++;
-            maxFreq = Math.max(maxFreq, freq[idx]);
-
-            while ((i - j + 1) - maxFreq > k) {
-                freq[s.charAt(j) - 'A']--;
-                j++;
-            }
-
-            res = Math.max(res, i - j + 1);
+        if (s == null || s.length() == 0) {
+            return 0;
         }
 
-        return res;
+        int n = s.length();
+        int start = 0;
+        Map<Character, Integer> freq = new HashMap<>();
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (!freq.containsKey(ch)) {
+                freq.put(ch, 1);
+            } else {
+                freq.put(ch, freq.get(ch) + 1);
+            }
+            while (countDistintChar(freq) > k) {
+                char startChar = s.charAt(start);
+                freq.put(startChar, freq.get(startChar) - 1);
+                start++;
+            }
+
+            maxLength = Math.max(maxLength, i - start + 1);
+
+        }
+
+        return maxLength;
+    }
+
+    private int countDistintChar(Map<Character, Integer> freq) {
+        int totalCharacter = 0;
+        int maxVal = 0;
+        for (Map.Entry<Character, Integer> ch : freq.entrySet()) {
+            totalCharacter += ch.getValue();
+            maxVal = Math.max(maxVal, ch.getValue());
+        }
+
+        return totalCharacter - maxVal;
     }
 }

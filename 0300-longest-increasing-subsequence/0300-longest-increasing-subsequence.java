9347a598c98720f55838b1ad3ee4a74711f95f45
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-10 10:08:34 +0530

    Time: 3 ms (95.82%), Space: 46.4 MB (27.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+        int n=nums.length;
+        List<Integer> res= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            binarySearch(res, nums[i]);
+        }
+        return res.size();
+    }
 
-        int maxVal = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                int take = 0;
-                if (nums[j] < nums[i]) {
-                    take = dp[j] + 1;
-                }
-                dp[i] = Math.max(take, dp[i]);
-                maxVal = Math.max(maxVal, dp[i]);
+    private void binarySearch(List<Integer> res, int val){
+        int n=res.size();
+        int idx=n;
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(res.get(mid)>=val){
+                idx=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
+
         }
 
-        return maxVal;
+        if(idx==n){
+            res.add(val);
+        }else{
+            res.set(idx, val);
+        }
+
     }
 }

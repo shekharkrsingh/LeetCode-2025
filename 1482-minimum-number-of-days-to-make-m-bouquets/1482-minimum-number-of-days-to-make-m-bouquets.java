026ae45819aaf3e3d5ebd6bdf2acda60b03fe106
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-12 16:58:36 +0530

    Time: 22 ms (20.77%), Space: 82.1 MB (95.15%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,51 +1,46 @@
 class Solution {
-
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-
-        if (n < (long) m * k)
-            return -1;
-
-        int minDay = bloomDay[0], maxDay = bloomDay[0];
+        int length = bloomDay.length;
+        int minDay = Integer.MAX_VALUE;
+        int maxDay = 0;
         for (int day : bloomDay) {
             minDay = Math.min(minDay, day);
             maxDay = Math.max(maxDay, day);
         }
-
-        int start = minDay, end = maxDay;
-        int ans = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (canMake(bloomDay, mid, m, k)) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return ans;
+        return computerMinDayForMBouquets(minDay, maxDay, bloomDay, m, k);
     }
 
-    private boolean canMake(int[] bloomDay, int day, int m, int k) {
-        int flowers = 0, bouquets = 0;
-
-        for (int bloom : bloomDay) {
-            if (bloom <= day) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                    if (bouquets >= m)
-                        return true; // early stop
-                }
+    private int computerMinDayForMBouquets(int minDay, int maxDay, int[] bloomDay, int m, int k) {
+        int result = -1;
+        while (minDay <= maxDay) {
+            int mid = minDay + (maxDay - minDay) / 2;
+            int noOfBounquets = dayRequired(mid, bloomDay, k);
+            if (noOfBounquets >= m) {
+                result = mid;
+                maxDay = mid - 1;
             } else {
-                flowers = 0;
+                minDay = mid + 1;
             }
         }
 
-        return false;
+        return result;
+    }
+
+    private int dayRequired(int day, int[] bloomDay, int k) {
+        int noOfBounquets = 0;
+        int flowerCount = 0;
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= day) {
+                flowerCount++;
+            } else {
+                flowerCount = 0;
+            }
+            if (flowerCount == k) {
+                noOfBounquets++;
+                flowerCount = 0;
+            }
+        }
+
+        return noOfBounquets;
     }
 }

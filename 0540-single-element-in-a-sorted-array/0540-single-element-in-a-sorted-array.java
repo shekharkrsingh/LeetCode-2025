eacc7c8af3657676ee53824ab09fc5ae29818ef6
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-12 13:54:35 +0530

    Time: 0 ms (100%), Space: 53 MB (20.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,30 +1,31 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
+        int length = nums.length;
+        if (length == 1) {
             return nums[0];
         }
         if (nums[0] != nums[1]) {
             return nums[0];
         }
-        if (nums[n - 2] != nums[n - 1]) {
-            return nums[n - 1];
+        if (nums[length - 1] != nums[length - 2]) {
+            return nums[length - 1];
         }
         int start = 1;
-        int end = n - 2;
+        int end = length - 2;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            if (nums[mid + 1] != nums[mid] && nums[mid - 1] != nums[mid]) {
                 return nums[mid];
             }
+
             if (mid % 2 != 0) {
-                if (nums[mid] == nums[mid - 1]) {
+                if (nums[mid - 1] == nums[mid]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             } else {
-                if (nums[mid] == nums[mid + 1]) {
+                if (nums[mid + 1] == nums[mid]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;

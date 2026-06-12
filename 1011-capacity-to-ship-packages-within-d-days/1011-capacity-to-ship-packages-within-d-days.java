4c7a3bc16e704a1be253d8516c2e44c72b1e7db7
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-12 21:11:27 +0530

    Time: 11 ms (69.27%), Space: 50 MB (86.24%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,43 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int n = weights.length;
-        int maxW = 0;
-        int totalW = 0;
-        for (int w : weights) {
-            maxW = Math.max(w, maxW);
-            totalW += w;
+        int length=weights.length;
+        int minWeight=0;
+        int maxWeight=0;
+        for(int weight: weights){
+            minWeight=Math.max(minWeight, weight);
+            maxWeight+=weight;
         }
-        int start = maxW;
-        int end = totalW;
-        return binarySearch(weights, days, start, end);
+        return computeWeight(minWeight, maxWeight, weights, days);
     }
 
-    private int binarySearch(int[] weights, int days, int start, int end) {
-        int minC = end;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isValid(weights, mid, days)) {
-                minC = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+    private int computeWeight(int minWeight, int maxWeight, int[] weights, int days){
+        int totalWeight=minWeight;
+        while(minWeight<=maxWeight){
+            int mid=minWeight+(maxWeight-minWeight)/2;
+            int daysRequired=noOfDays(mid, weights);
+            if(daysRequired<=days){
+                totalWeight=mid;
+                maxWeight=mid-1;
+            }else{
+                minWeight=mid+1;
             }
         }
-        return minC;
+        return totalWeight;
     }
 
-    private boolean isValid(int[] weights, int cap, int days) {
-        int n = weights.length;
-        int dayCnt = 1;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += weights[i];
-            if (sum > cap) {
-                dayCnt++;
-                sum = weights[i];
+    private int noOfDays(int weight, int[] weights){
+        int day=0;
+        int sumWeight=0;
+        for(int i=0;i<weights.length;i++){
+            if(sumWeight+weights[i]>weight){
+                day++;
+                sumWeight=0;
             }
+            sumWeight+=weights[i];
         }
-        return dayCnt <= days;
+        if(sumWeight>0){
+            day++;
+        }
+        return day;
     }
 }

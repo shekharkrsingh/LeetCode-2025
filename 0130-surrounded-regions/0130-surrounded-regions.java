98f0bbb3eded20f352df8b7dc609cfc42f785332
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-06-21 13:15:44 +0530

    Time: 4 ms (17.12%), Space: 47.5 MB (91.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,51 +1,46 @@
 class Solution {
-
-    private static int[] dx = { -1, 1, 0, 0 };
-    private static int[] dy = { 0, 0, -1, 1 };
-
+    private static int[] x= {1, -1, 0, 0};
+    private static int[] y= {0, 0, -1, 1};
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) && board[i][j] == 'O' && !visited[i][j]) {
-                    
-                    visited[i][j] = true;
-                    q.offer(new int[] { i, j });
+        int n=board.length;
+        int m= board[0].length;
+        Queue<int[]> q= new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O' ){
+                    q.offer(new int[]{i, j});
+                    board[i][j]='S';
                 }
             }
         }
-
-        bfs(n, m, board, q, visited);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O' && !visited[i][j]) {
-                    board[i][j] = 'X';
+        bfs(q, board, n, m);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]=='S'){
+                    board[i][j]='O';
+                }else{
+                    board[i][j]='X';
                 }
             }
         }
     }
 
-    private void bfs(int n, int m, char[][] board, Queue<int[]> q, boolean[][] visited) {
-        while (!q.isEmpty()) {
-            int[] node = q.poll();
-            int x = node[0];
-            int y = node[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 'O' && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[] { nx, ny });
+    private void bfs(Queue<int[]> q, char[][] board, int n, int m){
+        while(!q.isEmpty()){
+            int[] node= q.poll();
+            for(int i=0;i<4; i++){
+                int nx=node[0]+x[i];
+                int ny=node[1]+y[i];
+                if(isValid(nx, ny, n, m) && board[nx][ny]=='O'){
+                    q.offer(new int[]{nx, ny});
+                    board[nx][ny]='S';
                 }
             }
         }
     }
 
+
+    private boolean isValid(int x, int y, int n, int m){
+        return !(x<0 || y<0 || x==n || y==m);
+    }
 }

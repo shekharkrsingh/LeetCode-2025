504fadc7ca83feec2321f60924492ce66c21ab9b
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-04 19:37:51 +0530

    Time: 20 ms (8.31%), Space: 52.2 MB (13.97%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,56 +1,52 @@
 class Solution {
+
+    static final int MOD = 1_000_000_007;
+
     public int countPaths(int n, int[][] roads) {
-        List<List<int[]>> adj = adjList(n, roads);
 
-        long[] dis = new long[n];
-        long[] count = new long[n];
+        List<List<int[]>> adj = adjacencyList(n, roads);
         PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
-
+        long[] dis = new long[n];
         Arrays.fill(dis, Long.MAX_VALUE);
+        int[] cnt = new int[n];
         dis[0] = 0;
-        count[0] = 1;
-
+        cnt[0] = 1;
         pq.offer(new long[] { 0, 0 });
+        dijkstra(adj, pq, dis, cnt);
+        return cnt[n - 1];
+    }
 
-        int mod = 1_000_000_007;
+    private void dijkstra(List<List<int[]>> adj, PriorityQueue<long[]> pq, long[] dis, int[] cnt) {
 
         while (!pq.isEmpty()) {
-            long[] nodeArray = pq.poll();
-            int node = (int) nodeArray[0];
-            long d = nodeArray[1];
-
-            if (d > dis[node])
+            long[] cur = pq.poll();
+            int node = (int) cur[0];
+            long dist = cur[1];
+            if (dist > dis[node])
                 continue;
-
-            for (int[] arr : adj.get(node)) {
-                int next = arr[0];
-                long nd = d + arr[1];
-
-                if (nd < dis[next]) {
-                    dis[next] = nd;
-                    count[next] = count[node];
-                    pq.offer(new long[] { next, nd });
-                } else if (nd == dis[next]) {
-                    count[next] = (count[next] + count[node]) % mod;
+            for (int[] edge : adj.get(node)) {
+                int next = edge[0];
+                long newDist = dist + edge[1];
+                if (newDist < dis[next]) {
+                    dis[next] = newDist;
+                    cnt[next] = cnt[node];
+                    pq.offer(new long[] { next, newDist });
+                } else if (newDist == dis[next]) {
+                    cnt[next] = (cnt[next] + cnt[node]) % MOD;
                 }
             }
         }
-
-        return (int) (count[n - 1] % mod);
     }
 
-    private List<List<int[]>> adjList(int n, int[][] roads) {
+    private List<List<int[]>> adjacencyList(int n, int[][] roads) {
         List<List<int[]>> adj = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-
         for (int[] road : roads) {
             adj.get(road[0]).add(new int[] { road[1], road[2] });
             adj.get(road[1]).add(new int[] { road[0], road[2] });
         }
-
         return adj;
     }
 }

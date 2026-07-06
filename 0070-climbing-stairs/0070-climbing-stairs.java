8d1d07a7144bca04960afe9c3c7fa2e0d4a0aa39
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-06 23:43:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int first = 1, second = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = first + second;
-            second = first;
-            first = temp;
-        }
-        return first;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return countWay(n, dp);
+    }
+
+    private int countWay(int n, int[] dp) {
+        if (n == 1 || n == 2)
+            return n;
+        if (dp[n] != -1)
+            return dp[n];
+        dp[n] = countWay(n - 1, dp) + countWay(n - 2, dp);
+        return dp[n];
     }
 }

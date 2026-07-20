9b70dc1d2e1ea3be277451bc65bc3fd2fe8c199c
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-20 12:43:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,43 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp= new int[n];
-        if(n>m){
-            return uniquePaths(n, m);
-        }
-        Arrays.fill(dp, 1);
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                int up = 0;
-                int left = 0;
-                up = dp[j - 1];
-                left = dp[j];
-                dp[j] = left + up;
-            }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        return dp[n-1];
-
+        return traverseGrid(m - 1, n - 1, dp);
     }
 
-    private int sol(int m, int n, int[][] dp) {
-        if (m == 0 && n == 0) {
+    private int traverseGrid(int i, int j, int[][] dp) {
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        if (i == 0 && j == 0) {
             return 1;
         }
-        if (dp[m][n] != -1) {
-            return dp[m][n];
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
         int up = 0;
         int left = 0;
-
-        if (n - 1 >= 0) {
-            up = sol(m, n - 1, dp);
+        if (i - 1 >= 0) {
+            up = traverseGrid(i - 1, j, dp);
         }
-        if (m - 1 >= 0) {
-            left = sol(m - 1, n, dp);
+        if (j - 1 >= 0) {
+            left = traverseGrid(i, j - 1, dp);
         }
 
-        dp[m][n] = left + up;
-        return dp[m][n];
+        dp[i][j] = up + left;
+        return dp[i][j];
     }
 }

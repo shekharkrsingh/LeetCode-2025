ce920331ba8ed209d57ed2ca1a4d7ce95fe1bd64
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-29 15:59:48 +0530

    Time: 23 ms (31.8%), Space: 46 MB (78.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,42 +1,32 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
         int n = s.length();
-        int start = 0;
-        Map<Character, Integer> freq = new HashMap<>();
+        int[] freq = new int[26];
         int maxLength = 0;
-
+        int start = 0;
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if (!freq.containsKey(ch)) {
-                freq.put(ch, 1);
-            } else {
-                freq.put(ch, freq.get(ch) + 1);
-            }
-            while (countDistintChar(freq) > k) {
-                char startChar = s.charAt(start);
-                freq.put(startChar, freq.get(startChar) - 1);
+            freq[ch - 'A']++;
+            while (countDE(freq) > k ) {
+                char startC = s.charAt(start);
+                freq[startC-'A']--;
                 start++;
             }
-
             maxLength = Math.max(maxLength, i - start + 1);
-
         }
-
         return maxLength;
+
     }
 
-    private int countDistintChar(Map<Character, Integer> freq) {
-        int totalCharacter = 0;
-        int maxVal = 0;
-        for (Map.Entry<Character, Integer> ch : freq.entrySet()) {
-            totalCharacter += ch.getValue();
-            maxVal = Math.max(maxVal, ch.getValue());
+    private int countDE(int[] freq) {
+        int cnt = 0;
+        int max=0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                cnt+=freq[i];
+            }
+            max=Math.max(freq[i], max);
         }
-
-        return totalCharacter - maxVal;
+        return cnt-max;
     }
 }

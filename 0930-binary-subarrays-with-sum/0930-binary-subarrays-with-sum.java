a1a341f52526e8979a48765065d5199970cdf388
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-29 18:15:40 +0530

    Time: 3 ms (55.06%), Space: 50.8 MB (76.55%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return sol(nums, goal) - sol(nums, goal - 1);
+        if(goal==0){
+            return subArray(nums, goal);
+        }
+        return subArray(nums, goal)-subArray(nums, goal-1);
+
     }
 
-    private int sol(int[] nums, int goal) {
-        if (goal < 0 || nums == null) {
-            return 0;
-        }
-
-        int n = nums.length;
-        int start = 0;
-        int cnt = 0;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            while (sum > goal) {
-                sum -= nums[start];
+    private int subArray(int[] nums, int goal){
+        int n=nums.length;
+        int cnt=0;
+        int start=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            while(sum>goal){
+                sum-=nums[start];
                 start++;
             }
-            cnt += i - start + 1;
+            cnt+=i-start+1;
         }
 
         return cnt;

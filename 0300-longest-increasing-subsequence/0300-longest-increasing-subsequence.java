a2968b7c7b7f452f48e7d9b8dd6c911966d2ae5a
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2026-07-31 21:27:53 +0530

    Time: 405 ms (6.87%), Space: 237.4 MB (7.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,34 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        List<Integer> res= new ArrayList<>();
-        for(int i=0;i<n;i++){
-            binarySearch(res, nums[i]);
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return res.size();
+
+        return findLISLength(0, -1, nums, dp);
     }
 
-    private void binarySearch(List<Integer> res, int val){
-        int n=res.size();
-        int idx=n;
-        int start=0;
-        int end=n-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(res.get(mid)>=val){
-                idx=mid;
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
-
+    private int findLISLength(int i, int j, int[] nums, int[][] dp) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (dp[i][j + 1] != -1) {
+            return dp[i][j + 1];
         }
 
-        if(idx==n){
-            res.add(val);
-        }else{
-            res.set(idx, val);
+        int take = 0;
+        if (j == -1 || nums[i] > nums[j]) {
+            take = findLISLength(i + 1, i, nums, dp) + 1;
         }
+        int notTake = findLISLength(i + 1, j, nums, dp);
 
+        dp[i][j + 1] = Math.max(take, notTake);
+        return dp[i][j + 1];
     }
 }
